fc0bfdfd2b1cd2ef4b58f00bd56c50f25f61c53e
Aryan Goyal <aryangoyal90@outlook.com>
2024-08-20 16:31:34 +0530

    Create q1.py

---
+++ Assignment4/q1.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# Z =np.arange(50)  # create a vector from 0 to n - 1 ;   
+# Z = np.arange(10, 50) # create a vector of numbers ranging from 10 - 49
+# Z = Z[::-1]  # To reverse a vector
+# Z = np.eye(3) # create 3 X3 Matrix
+# nz = np.nonzero([1,2,0,0,4,0]) # gives the non-zero elements index 
+# Z = np.random.random((3,3,3)) # (3) -> An array with 3 random elements || (3,3) - An array of 3 SubArrays with 3 elements each || (3,3,3) - An Array of # subarrays with consists oif 3 more arrays with 3 elements each!
+# Z = np.random.random((10,10))
+# Zmin, Zmax = Z.min(), Z.max() # Gives min and max elements from this Z array!!
+# print(Zmin, Zmax)
+# Z = np.random.random(30)
+# m = Z.mean() # Gives the mean of the array!!
+# print(m)
+# Z = np.ones((10,10)) # 2d array
+# Z[1:-1,1:-1] = 0 # Creates 1 at border and 0 inside of an 2d array!!
+# Z = np.ones((5,5)) # creates an 2d array with ones!!
+# Z = np.pad(Z, pad_width=1, mode='constant', constant_values=0) # Works same as padding parameters = (array, pad_width, mode, contant_values);
+# Z = np.dot(np.ones((4,3)), np.ones((3,2))) # Dot product of two matrix (4,3). (3,2)
+# Z = np.arange(11) # a range of the array
+# Z[(3 < Z) & (Z <= 8)] *= -1 # condition of array!! 
+#randint = random integer
+# Z1 = np.random.randint(0,10,10)
+# Z2 = np.random.randint(0,10,10)
+# print(np.intersect1d(Z1,Z2)) # find common values
+
+
+
+print(Z)
